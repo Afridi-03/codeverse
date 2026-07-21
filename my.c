6a0da587f9871a0a3a0139a0d4883c9ae29f0bf1
Afridi Hasan <mdafridihasan55@gmail.com>
2026-07-21 13:53:41 +0600

    Add files via upload

--- my.c
+++ my.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
 int main() {
-    
-    char s[100];
-    
-    scanf("%[^\n]%*c", s);
-      
-    printf("Hello, World!\n");
-    printf("%s", s);
-    
+    char s[100], ch, sen[100];
+
+    scanf("%c", &ch);
+    scanf("%s",&s);
+    scanf(" %[^\n]", sen);
+
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+
     return 0;
 }
